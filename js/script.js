171c0fee6c752e50a48fcac169ffba3d7d98003c
dmaharika <dmaharika@gmail.com>
2023-04-13 14:28:18 +0700

    fixed the dark-light theme

--- js/script.js
+++ js/script.js
@@ -1,43 +1,16 @@
-const themeToggler = document.querySelector('.theme-toggler');
-const main = document.querySelector('.main');
-var isDark = localStorage.getItem('isDark');
+const themeToggler = document.querySelector(".theme-toggler");
+const main = document.querySelector(".main");
+const currentTheme = localStorage.getItem("theme");
 
-if (isDark === null) {
-    localStorage.setItem('isDark', 'false');
+if (currentTheme === "dark") {
+    main.classList.add("dark-mode");
 }
 
-themeToggler.addEventListener('click', function() {
-  if (isDark) {
-    main.classList.remove('dark-mode')
-    isDark = false;
-    localStorage.setItem('isDark', 'false');
-  } else {
-    main.classList.add('dark-mode');
-    isDark = true;
-    localStorage.setItem('isDark', 'true');
-  }
+themeToggler.addEventListener("click", function() {
+    main.classList.toggle("dark-mode");
+    if (main.classList.contains("dark-mode")) {
+        localStorage.setItem("theme", "dark");
+    } else {
+        localStorage.setItem("theme", "light");
+    }
 });
-
-// const themeToggler = document.querySelector('.theme-toggler');
-// const main = document.querySelector('.main');
-
-// // Check if the user has set a theme preference before
-// const currentTheme = localStorage.getItem('theme');
-// if (currentTheme === 'dark') {
-//   main.classList.add('dark-mode');
-// } else{
-//     currentTheme = 'light'
-// }
-
-// themeToggler.addEventListener('click', function() {
-//   // Toggle the 'dark-mode' class on the <body> element
-//   main.classList.toggle('dark-mode');
-
-//   // Store the current theme preference in local storage
-//   if (main.classList.contains('dark-mode')) {
-//     localStorage.setItem('theme', 'dark');
-
-//   } else {
-//     localStorage.setItem('theme', 'light');
-//   }
-// });
